5b380d2472c652ebedbe291000a1bbd689f74ac9
TCO932 <64690879+TCO932@users.noreply.github.com>
2023-02-15 13:37:45 +0400

    lab1

---
+++ lab1/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Введите количество капель: ");
+int amount = Convert.ToInt32(Console.ReadLine());
+Double horseMass = 420000.0;
+Double mouseMass = 19.0;
+int calcMices(int amount) {
+    return (int)Math.Truncate((horseMass * amount) / mouseMass);
+}
+Console.WriteLine($"Если средняя масса лошади 420кг, а средняя масса мыши 19г, то {amount} капель убьет {calcMices(amount)} мышей");
+
