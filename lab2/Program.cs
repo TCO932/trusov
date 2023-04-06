12aef7f09505ef268a1c92c29048207da40e3dcc
anton <chirkof.02@gmail.com>
2023-04-06 13:20:57 +0400

    init

---
+++ lab2/Program.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine(@"
+Выберите операцию:\n
+1) Сложение
+2) Вычитание
+3) Умножение
+4) Деление
+");
+int operation = Convert.ToInt16(Console.ReadLine());
+
+Console.WriteLine("Введите первое число");
+double a = Convert.ToDouble(Console.ReadLine());
+
+
+Console.WriteLine("Введите второе число");
+double b = Convert.ToDouble(Console.ReadLine());
+
+string sign = "";
+double result = -1;
+switch (operation)
+{
+    case 1: 
+        sign = "+";
+        result = a + b;
+        break;
+    case 2: 
+        sign = "-";
+        result = a - b;
+        break;
+    case 3: 
+        sign = "*";
+        result = a * b;
+        break;
+    case 4: 
+        sign = "/";
+        result = a / b;
+        break;
+    default:
+        Console.WriteLine("Неправильный выбор операции");
+        return;
+}
+
+Console.WriteLine($"{a} {sign} {b} = {result}");
+
+
+
